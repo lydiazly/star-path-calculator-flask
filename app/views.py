33ba38c636194e5482ad2b5a70d5a59815feed97
Lydia <lydiazly2@gmail.com>
2024-07-02 14:34:49 -0700

    Organized code; fixed text

--- app/views.py
+++ app/views.py
@@ -1,38 +1,26 @@
 # app/views.py
 from flask import request, jsonify, render_template
 from app import app
-from app.utils import get_equinoxes, get_solstices
-from skyfield.api import load
+from core import get_coords
 
-@app.route('/get_coords', methods=['GET'])
-def get_coords():
+@app.route('/coords', methods=['GET'])
+def coords():
     year = request.args.get('year')
     
     if year is None:
-        return jsonify({'error': 'Year is required'}), 400
+        return jsonify({'error': 'Year is required.'}), 400
 
     try:
         year = int(year)
     except ValueError:
-        return jsonify({'error': 'Year must be an integer'}), 400
-
-    ts = load.timescale()
-    t = ts.ut1(year, 1, 1, 12, 0, 0)
+        return jsonify({'error': 'Year must be an integer.'}), 400
 
     try:
-        vernal_ra_j2000, vernal_dec_j2000, autumnal_ra_j2000, autumnal_dec_j2000 = get_equinoxes(t)
-        summer_ra_j2000, summer_dec_j2000, winter_ra_j2000, winter_dec_j2000 = get_solstices(t)
+        results = get_coords(year)
     except Exception as e:
-        return jsonify({'error': f'Error in calculation: {e}'}), 500
+        return jsonify({'error': f'Error: {e}'}), 500
 
-    results = {
-        'vernal_ra': str(vernal_ra_j2000), 'vernal_dec': str(vernal_dec_j2000),
-        'autumnal_ra': str(autumnal_ra_j2000), 'autumnal_dec': str(autumnal_dec_j2000),
-        'summer_ra': str(summer_ra_j2000), 'summer_dec': str(summer_dec_j2000),
-        'winter_ra': str(winter_ra_j2000), 'winter_dec': str(winter_dec_j2000),
-    }
-
-    return jsonify({'year': year, 'results': results})
+    return jsonify({'year': year, 'results': results}), 200
 
 @app.route('/')
 def home():
---
+++ core/__init__.py
@@ -0,0 +1,2 @@
+# core/__init__.py
+from .coordinates import get_coords
