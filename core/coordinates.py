9a4e08bd72030761f6041e3da2b56379796256c0
lydiazly <46819274+lydiazly@users.noreply.github.com>
2024-07-04 11:56:10 -0700

    Organized code structure (#1)
    * Organized code; fixed text
    
    * Moved data file to data/
    
    * Organized funcs

---
+++ core/coordinates.py
@@ -0,0 +1,81 @@
+# core/coordinates.py
+from typing import Tuple
+from skyfield.api import load, Star, Angle, Timescale
+from skyfield.framelib import ecliptic_frame
+from scipy.optimize import root
+import os
+
+DATA_DIR = 'data'
+DATA_FILE = 'de406.bsp'
+
+# Load the ephemeris data (global)
+if not os.path.exists(DATA_DIR):
+    os.makedirs(DATA_DIR)
+os.chdir(DATA_DIR)
+eph = load(DATA_FILE)
+earth = eph['earth']
+os.chdir('..')
+
+
+# The basic idea of determining the ICRS coordinates of equinoxes of date is to
+# find the points which have the of-date ecliptic coordinates of (0,0), (180,0).
+# For solstices, it is to
+# find the points which have the of-date ecliptic coordinates of (90,0), (270,0).
+def get_ecliptic_coord(positions_j2000, ts: Timescale) -> Tuple[Angle]:
+    ra_j2000, dec_j2000 = positions_j2000
+    star = Star(ra_hours=ra_j2000/15, dec_degrees=dec_j2000)
+    position = earth.at(ts).observe(star)
+    lat, lon, _ = position.frame_latlon(ecliptic_frame)
+    return (lat.degrees, lon._degrees)
+
+
+def coord_for_equinoxes(positions_j2000, ts: Timescale) -> Tuple[Angle]:
+    a, b = get_ecliptic_coord(positions_j2000, ts)
+    return (a, b-180)
+
+
+def coord_for_solstices(positions_j2000, ts: Timescale) -> Tuple[Angle]:
+    a, b = get_ecliptic_coord(positions_j2000, ts)
+    return (a, b-90)
+
+
+def get_equinoxes(ts: Timescale) -> Tuple[Angle]:
+    sol = root(coord_for_equinoxes, x0=[180,0], args=(ts), method='lm', tol=1e-8)
+    x1 = sol.x[0]
+    x2 = sol.x[1]
+    autumnal_ra_j2000 = Angle(hours=x1/15)
+    autumnal_dec_j2000 = Angle(degrees=x2)
+    if x1 - 180 >= 0:
+        vernal_ra_j2000 = Angle(hours=(x1 - 180)/15)
+    else:
+        vernal_ra_j2000 = Angle(hours=(x1 + 180)/15)
+    vernal_dec_j2000 = Angle(degrees=-x2)
+    return (vernal_ra_j2000, vernal_dec_j2000, autumnal_ra_j2000, autumnal_dec_j2000)
+
+
+def get_solstices(ts: Timescale) -> Tuple[Angle]:
+    sol = root(coord_for_solstices, x0=[90,0], args=(ts), method='lm', tol=1e-8)
+    x1 = sol.x[0]
+    x2 = sol.x[1]
+    summer_ra_j2000 = Angle(hours=x1/15)
+    summer_dec_j2000 = Angle(degrees=x2)
+    winter_ra_j2000 = Angle(hours=(x1 + 180)/15)
+    winter_dec_j2000 = Angle(degrees=-x2)
+    return (summer_ra_j2000, summer_dec_j2000, winter_ra_j2000, winter_dec_j2000)
+
+
+def get_coords(year: int) -> dict:
+    ts = load.timescale()
+    t = ts.ut1(year, 1, 1, 12, 0, 0)
+
+    vernal_ra_j2000, vernal_dec_j2000, autumnal_ra_j2000, autumnal_dec_j2000 = get_equinoxes(t)
+    summer_ra_j2000, summer_dec_j2000, winter_ra_j2000, winter_dec_j2000 = get_solstices(t)
+
+    results = {
+        'vernal_ra': str(vernal_ra_j2000), 'vernal_dec': str(vernal_dec_j2000),
+        'autumnal_ra': str(autumnal_ra_j2000), 'autumnal_dec': str(autumnal_dec_j2000),
+        'summer_ra': str(summer_ra_j2000), 'summer_dec': str(summer_dec_j2000),
+        'winter_ra': str(winter_ra_j2000), 'winter_dec': str(winter_dec_j2000),
+    }
+
+    return results
--- get_equinoxes_solstices.py
+++ get_equinoxes_solstices.py
@@ -1,90 +1,32 @@
-from skyfield.api import load, Star, Angle
-from skyfield.framelib import ecliptic_frame
-import numpy as np
-from scipy.optimize import root
+'''
+The main script to calculate and print coordinates in the console.
+'''
+
 import argparse
-
-
-# Load the ephemeris data
-eph = load('de406.bsp')
-earth = eph['earth']
-
-
-# The basic idea of determining the ICRS coordinates of equinoxes of date is to
-# find the points which have the of-date ecliptic coordinates of (0,0), (180,0).
-# For solstices, it is to
-# find the points which have the of-date ecliptic coordinates of (90,0), (270,0).
-def get_ecliptic_coord(positions_j2000, ts):
-    ra_j2000, dec_j2000 = positions_j2000
-    star = Star(ra_hours=ra_j2000/15, dec_degrees=dec_j2000)
-    position = earth.at(ts).observe(star)
-    lat, lon, _ = position.frame_latlon(ecliptic_frame)
-    return [lat.degrees, lon._degrees]
-
-
-def used_for_equinoxes(positions_j2000, ts):
-    a, b = get_ecliptic_coord(positions_j2000, ts)
-    return [a, b-180]
-
-
-def used_for_solstices(positions_j2000, ts):
-    a, b = get_ecliptic_coord(positions_j2000, ts)
-    return [a, b-90]
-
-
-def get_equinoxes(ts):
-    sol = root(used_for_equinoxes, x0=[180,0], args=(ts), method='lm', tol=1e-8)
-    x1 = sol.x[0]
-    x2 = sol.x[1]
-    autumnal_ra_j2000 = Angle(hours=x1/15)
-    autumnal_dec_j2000 = Angle(degrees=x2)
-    if x1 - 180 >= 0:
-        vernal_ra_j2000 = Angle(hours=(x1 - 180)/15)
-    else:
-        vernal_ra_j2000 = Angle(hours=(x1 + 180)/15)
-    vernal_dec_j2000 = Angle(degrees=-x2)
-    return [vernal_ra_j2000, vernal_dec_j2000, autumnal_ra_j2000, autumnal_dec_j2000]
-
-
-def get_solstices(ts):
-    sol = root(used_for_solstices, x0=[90,0], args=(ts), method='lm', tol=1e-8)
-    x1 = sol.x[0]
-    x2 = sol.x[1]
-    summer_ra_j2000 = Angle(hours=x1/15)
-    summer_dec_j2000 = Angle(degrees=x2)
-    winter_ra_j2000 = Angle(hours=(x1 + 180)/15)
-    winter_dec_j2000 = Angle(degrees=-x2)
-    return [summer_ra_j2000, summer_dec_j2000, winter_ra_j2000, winter_dec_j2000]
-
+from datetime import datetime
+from core import get_coords
 
 def main():
-    
-    # Set up argument parser
     parser = argparse.ArgumentParser()
-    parser.add_argument('year', type=int)
+    parser.add_argument('year', type=int, nargs='?', default=datetime.now().year, help="Format: yyyy (default: %(default)s)")
     
     year = parser.parse_args().year
-    month = 1
-    day = 1
-    hour = 12
-    minute = 0
-    second = 0
-
-    ts = load.timescale()
-    t = ts.ut1(year, month, day, hour, minute, second)
     
-    vernal_ra_j2000, vernal_dec_j2000, autumnal_ra_j2000, autumnal_dec_j2000 = get_equinoxes(t)
-    summer_ra_j2000, summer_dec_j2000, winter_ra_j2000, winter_dec_j2000 = get_solstices(t)
+    try:
+        results = get_coords(year)
 
-    print (f'The ICRS coordinates (J2000) of equinoxes and solstices in the year {year} is:')
-    print ('Vernal Equinox:')
-    print (f'ra = {vernal_ra_j2000}, dec = {vernal_dec_j2000}')
-    print ('Autumnal Equinox:')
-    print (f'ra = {autumnal_ra_j2000}, dec = {autumnal_dec_j2000}')
-    print ('Summer solstice:')
-    print (f'ra = {summer_ra_j2000}, dec = {summer_dec_j2000}')
-    print ('Winter solstice:')
-    print (f'ra = {winter_ra_j2000}, dec = {winter_dec_j2000}')
+        print (f'The ICRS coordinates (J2000) of equinoxes and solstices in the year {year} are:')
+        print ('Vernal Equinox:')
+        print (f'  ra = {results["vernal_ra"]}, dec = {results["vernal_dec"]}')
+        print ('Autumnal Equinox:')
+        print (f'  ra = {results["autumnal_ra"]}, dec = {results["autumnal_dec"]}')
+        print ('Summer solstice:')
+        print (f'  ra = {results["summer_ra"]}, dec = {results["summer_dec"]}')
+        print ('Winter solstice:')
+        print (f'  ra = {results["winter_ra"]}, dec = {results["winter_dec"]}')
+    
+    except Exception as e:
+        print(f'Error: {e}')
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
