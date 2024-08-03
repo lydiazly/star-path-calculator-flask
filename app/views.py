55bc46689b0d98fdc55a81eea8b928406964f093
Lydia <lydiazly2@gmail.com>
2024-08-03 16:13:01 -0700

    added cal

--- app/views.py
+++ app/views.py
@@ -26,6 +26,7 @@ def ratelimit_error(e):
 @app.route("/equinox", methods=["GET"])
 @limiter.limit("6/second", override_defaults=False)
 def equinox():
+    # [Gregorian]
     year   = request.args.get("year", default=None, type=int)
     # month  = request.args.get("month", default=1, type=int)
     # day    = request.args.get("day", default=1, type=int)
@@ -56,22 +57,23 @@ def equinox():
 @app.route("/diagram", methods=["GET"])
 @limiter.limit("4/second", override_defaults=False)
 def diagram():
+    lat   = request.args.get("lat", default=None, type=float)
+    lng   = request.args.get("lng", default=None, type=float)
     year  = request.args.get("year", default=None, type=int)
     month = request.args.get("month", default=1, type=int)
     day   = request.args.get("day", default=1, type=int)
-    lat   = request.args.get("lat", default=None, type=float)
-    lng   = request.args.get("lng", default=None, type=float)
     flag  = request.args.get("flag", default=None)
+    cal   = request.args.get("cal", default="g")
     name  = request.args.get("name", default=None)
     hip   = request.args.get("hip", default=-1, type=int)
     ra    = request.args.get("ra", default=None, type=float)
     dec   = request.args.get("dec", default=None, type=float)
 
-    if year is None:
-        return jsonify({"error": "The year must be provided."}), 400
-
     if lat is None or lng is None:
-        return jsonify({"error": "The longitude and latitude must be provided."}), 400
+        return jsonify({"error": "Either longitude or latitude is not provided."}), 400
+
+    if year is None:
+        return jsonify({"error": "Year is not provided."}), 400
 
     if name:
         obj = {"name": name.lower()}
@@ -80,20 +82,21 @@ def diagram():
     elif ra is not None and dec is not None:
         obj = {"radec": (ra, dec)}
     else:
-        return jsonify({"error": "Either planet name, Hipparchus catalogue number, or (ra, dec) must be provided."}), 400
+        return jsonify({"error": "Either planet name, Hipparchus catalogue number, or (ra, dec) is not provided."}), 400
 
     # Get the equinox/solstice times
-    eqx_sol_keys = {
-        "ve": "vernal_time",
-        "ss": "summer_time",
-        "ae": "autumnal_time",
-        "ws": "winter_time"
-    }
     eqx_sol_time = []
-    if flag in eqx_sol_keys:
-        eqx_sol_time = get_coords(year)[eqx_sol_keys[flag]]  # [int, int, int, int, int, float]
-        month = eqx_sol_time[1]
-        day = eqx_sol_time[2]
+    if flag is not None:
+        eqx_sol_keys = {
+            "ve": "vernal_time",
+            "ss": "summer_time",
+            "ae": "autumnal_time",
+            "ws": "winter_time"
+        }
+        if flag in eqx_sol_keys:
+            eqx_sol_time = get_coords(year)[eqx_sol_keys[flag]]  # [int, int, int, int, int, float]
+            month = eqx_sol_time[1]
+            day = eqx_sol_time[2]
 
     try:
         results = get_diagram(year, month, day, lat=lat, lng=lng, **obj)
@@ -101,12 +104,13 @@ def diagram():
         return jsonify({"error": str(e)}), 500
 
     return jsonify({
+        "lat":         str(lat),
+        "lng":         str(lng),
         "year":        str(year),
         "month":       str(month),
         "day":         str(day),
         "flag":        flag,
-        "lat":         str(lat),
-        "lng":         str(lng),
+        "cal":         cal,
         "name":        name,
         "hip":         hip,
         "ra":          ra,
