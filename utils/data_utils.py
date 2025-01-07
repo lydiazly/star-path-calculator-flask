ed16abd1774062dfdd59a030ec9f9099ef2820af
Lydia <lydiazly2@gmail.com>
2025-01-06 17:42:47 -0800

    organized data_utils code

--- utils/data_utils.py
+++ utils/data_utils.py
@@ -15,12 +15,17 @@ data_dir = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__
 
 
 def convert_id(id_value):
-    # a --> alf
-    # e --> eps
-    # m --> mu.
-    # m1 --> mu.01
-    # 38 --> 38
-    # V640 --> V640
+    """
+    Converts Bayer Designation IDs for consistency.
+
+    Examples:
+      a --> alf
+      e --> eps
+      m --> mu.
+      m1 --> mu.01
+      38 --> 38
+      V640 --> V640
+    """
     import json
     with open(os.path.join(data_dir, 'greek_map.json'), 'r') as file:
         greek_map = json.load(file)
@@ -31,20 +36,25 @@ def convert_id(id_value):
     result = []
     num_str = ""
     for char in id_value:
+        # Convert lowercase Latin letters to Greek abbreviation (no 'f', 'j', and 'v' found in the source file)
         if char.islower():
-            result.append(greek_map.get(char, char))  # Convert lowercase to Greek abbreviation
+            result.append(greek_map.get(char, char))
+        # Collect digits
         elif char.isdigit():
-            num_str += char  # Collect digits into num_str
+            num_str += char
+        # Retain uppercase Latin letters and other characters
         else:
-            result.append(char)  # Keep uppercase and other characters unchanged
+            result.append(char)
 
+    # Pad the numeric part to 2 digits
     if num_str:
-        result.append(f"{int(num_str):02d}")  # Pad numeric part to 2 digits
+        result.append(f"{int(num_str):02d}")
 
     return "".join(result)
 
 
 def merge_pinyin_with_apostrophe(pinyin_word_list):
+    """Helper function to join Pinyin characters."""
     vowels = {'a', 'e', 'i', 'o', 'u'}
     result = pinyin_word_list[0].title()
     for i in range(1, len(pinyin_word_list)):
@@ -53,7 +63,9 @@ def merge_pinyin_with_apostrophe(pinyin_word_list):
         result += pinyin_word_list[i].title() if pinyin_word_list[i-1][0] == '(' else pinyin_word_list[i]
     return result
 
+
 def process_text(text):
+    """Helper function to process a Pinyin word."""
     from pypinyin import pinyin, load_phrases_dict, Style
     # from pypinyin_dict.phrase_pinyin_data import large_pinyin
     from pypinyin_dict.phrase_pinyin_data import zdic_cibs
@@ -71,6 +83,7 @@ def process_text(text):
 
 
 def get_pinyin(text):
+    """Converts a word to Pinyin."""
     if pd.isnull(text) or text == '':
         return ''
 
@@ -84,6 +97,7 @@ def get_pinyin(text):
 
 
 def load_name_zh() -> pd.DataFrame:
+    """Reads the table containing Traditional Chinese names then add Simplified Chinese and Pinyin columns."""
     df = pd.read_csv(os.path.join(data_dir, 'star-name-zh.csv'))
     df.sort_values(by=['Const', 'Id', 'Name_en'], inplace=True)
     df.reset_index(drop=True, inplace=True)
@@ -108,20 +122,33 @@ def load_name_zh() -> pd.DataFrame:
 
 
 def load_hip_ident() -> pd.DataFrame:
+    """
+    Merges proper names and Bayer Designations with Chinese names.
+
+    Does the following:
+    1. Read proper names, Bayer Designations, and Chinese names from files.
+    2. Correct Bayer Designations.
+    3. Group by HIP and merge on HIP.
+    4. Combine names into a single 'name' column.
+    5. Save to 'hip_ident_zh.csv'.
+    """
     import numpy as np
+
+    # Read proper names and Bayer Designations --------------------------------|
     df_bayer = pd.read_pickle(os.path.join(data_dir, 'ident_bayer.pkl'))
-    df_bayer_add = pd.read_csv(os.path.join(data_dir, 'ident_bayer_add.csv'))
+    df_bayer_add = pd.read_csv(os.path.join(data_dir, 'ident_bayer_add.csv'))  # additional table
     df_proper = pd.read_pickle(os.path.join(data_dir, 'ident_proper.pkl'))
     # df_to_csv(df_bayer, 'ident_bayer.csv')
     # df_to_csv(df_proper, 'ident_proper.csv')
 
-    # df_name_zh = load_name_zh()
+    # Read Chinese names ------------------------------------------------------|
+    # df_name_zh_matched = load_name_zh()  # also save to 'bayer-zh.csv'
     df_name_zh_matched = pd.read_csv(os.path.join(data_dir, 'bayer-zh.csv'))
     df_name_zh_matched['HIP'] = ''
     df_name_zh_matched['Mark'] = ''
     df_name_zh_columns = df_name_zh_matched.columns.tolist()
 
-    # Correct Bayer Designations (first match)
+    # Correct Bayer Designations (first match) --------------------------------|
     df_name_zh_corrections = pd.read_csv(os.path.join(data_dir, 'bayer-zh-corrections.csv'))
     df_name_zh_corrections.set_index('Name_zh_hk', inplace=True)
     df_name_zh_matched.set_index('Name_zh_hk', inplace=True)
@@ -142,13 +169,15 @@ def load_hip_ident() -> pd.DataFrame:
                 df_name_zh_matched.at[idx, 'HIP'] = matches.iloc[0]['HIP']
             else:
                 df_name_zh_matched.at[idx, 'Mark'] = 'NA'  # No match found
+    # Save the intermediate table containing Chinese names and Bayer Designations
     df_to_csv(df_name_zh_matched, 'bayer-zh-hip.csv')
 
-    # Group by HIP and aggregate Bayer Designation and Proper Name with '/'
+    # Group by HIP ------------------------------------------------------------|
+    # Aggregate Bayer Designation and Proper Name with '/'
     df_bayer_agg = df_bayer.groupby('HIP')['Bayer Designation'].apply(lambda x: '/'.join(x)).reset_index()
     df_proper_agg = df_proper.groupby('HIP')['Proper Name'].apply(lambda x: '/'.join(x)).reset_index()
 
-    # Merge the DataFrames on the HIP column
+    # Merge the DataFrames on the HIP column ----------------------------------|
     df_merged = pd.merge(df_bayer_agg, df_proper_agg, on='HIP', how='outer')
     # df_merged = pd.merge(df_merged, df_name_zh_matched[['HIP', 'Name_zh_hk', 'Name_zh', 'Pinyin']], on='HIP', how='left')
     df_name_zh_filtered = df_name_zh_matched[df_name_zh_matched['HIP'].notnull()]
@@ -161,6 +190,7 @@ def load_hip_ident() -> pd.DataFrame:
         (~df_merged['HIP'].isin(df_merged.dropna(subset=['Bayer Designation'])['HIP'])),
         df_merged['Bayer Designation_new'], df_merged['Bayer Designation']
     )
+
     # Drop the temporary 'Bayer Designation_new' column
     df_merged.drop(columns=['Bayer Designation_new'], inplace=True)
 
@@ -171,14 +201,15 @@ def load_hip_ident() -> pd.DataFrame:
     df_merged['Name_zh'] = df_merged['Name_zh'].fillna('')
     df_merged['Pinyin'] = df_merged['Pinyin'].fillna('')
 
-    # Combine names into a single 'name' column
+    # Combine names into a single 'name' column -------------------------------|
     df_merged['name'] = df_merged['Bayer Designation'] + '/' + df_merged['Proper Name']
     df_merged['name'] = df_merged['name'].str.strip('/').str.replace('//', '/')
 
-    # Select only the required columns and rename them
+    # Select only the required columns and rename them ------------------------|
     df_merged = df_merged[['HIP', 'name', 'Name_zh', 'Name_zh_hk', 'Pinyin']]
     df_merged.columns = ['hip', 'name', 'name_zh', 'name_zh_hk', 'pinyin']
 
+    # Output ------------------------------------------------------------------|
     df_to_csv(df_merged, 'hip_ident_zh.csv')
 
     return df_merged
@@ -243,3 +274,29 @@ def hip_validation(hip_min=None, hip_max=None):
     valid: 117930
     mag_nan: 0, ra_nan: 254, dec_nan: 254
     """
+
+
+def docx_to_pkl():
+    """Reads docx files and saves the entries to pkl files."""
+    from docx import Document
+
+    col_name = "Proper Name"
+    doc_name = "ident6.docx"
+    out_name = "ident_proper.pkl"
+
+    # col_name = "Bayer Designation"
+    # doc_name = "ident4.docx"
+    # out_name = "ident_bayer.pkl"
+
+    doc = Document(os.path.join(data_dir, doc_name))
+    full_text = []
+    for para in doc.paragraphs:
+        full_text.append(para.text)
+
+    df = pd.DataFrame(columns={col_name: str, "HIP": int})
+
+    for i in range(len(full_text)):
+        a, b = full_text[i].split('|')
+        df.loc[i] = [a.strip(), int(b.strip())]
+
+    df.to_pickle(os.path.join(data_dir, out_name))
