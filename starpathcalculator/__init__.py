507f35569419e914625508313f6904775f5d44cc
Lydia <lydiazly2@gmail.com>
2026-05-07 00:12:17 -0700

    fix __version__ when not installing as a pkg

--- starpathcalculator/__init__.py
+++ starpathcalculator/__init__.py
@@ -1,8 +1,12 @@
 # __init__.py
 """Tracks the apparent motions of a celestial object observed from any location on any chosen date."""
 
-from importlib.metadata import version
-
 __all__ = ['run']
-__version__ = version('star-path-calculator')
 __author__ = 'Stardial'
+
+try:
+    from importlib.metadata import version, PackageNotFoundError
+
+    __version__ = version('star-path-calculator')
+except PackageNotFoundError:
+    __version__ = 'dev'
